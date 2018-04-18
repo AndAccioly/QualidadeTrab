8e2c6155531ef3fb303ddee8bae10e0505311bdf
André Accioly <andreacciolyl@hotmail.com>
2018-04-18 14:55:26 -0300

    Primeiro commit
    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
+import java.lang.Runtime;
 
 public class Main{
-	public static void main(String[] args)}{
+	public static void main(String[] args){
 		Scanner reader = new Scanner(System.in);
 		int opcao = -1;
 
 		while(opcao != 0){
 			System.out.println("Loja de livros");	
-			while(opcao != 0 && opcao != 1){
-				System.out.println("0. Sair");
-				System.out.println("1. Teste");
-				opcao = reader.nextInt();
-			}
+			System.out.println("0. Sair");
+			System.out.println("1. Teste");
+			opcao = reader.nextInt();
 			
 			
 		}
