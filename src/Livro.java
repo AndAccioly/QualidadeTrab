8e56302b4271a8ef12fe993d04f8567e6ddbf880
Kfourit <kfourit@gmail.com>
2018-06-17 19:22:55 -0300

    Comentados Livro.java e Pessoa.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,6 +1,12 @@
-//Acerca de cada livro, serão armazenados os seguintes dados: título, nome do autor, data de publicação, código e gênero literário
-
-
+/**
+* Classe que representa o objeto Livro. Acerca de cada livro, serão armazenados os seguintes dados: 
+* título, nome do autor, data de publicação, código, gênero literário e numero de livros
+*
+* @author Andre Accioly
+* @author Tiago Kfouri
+* @version %I%, %G%
+* @since 1.0
+*/
 class Livro{
 	private String titulo;
 	private String nomeAutor;
@@ -9,6 +15,12 @@ class Livro{
 	private String genero;
 	private int quantidade;
 
+	/**
+	* Construtor da classe Livro. Caso nao seja passado parametro, os campos serao iniciados com campos genericos
+	* 
+	* @see Livro
+	* @since 1.0
+	*/
 	public Livro(){
 		this.titulo = "Titulo generico";
 		this.nomeAutor = "Nome autor generico";
@@ -19,6 +31,18 @@ class Livro{
 
 	}
 
+	/**
+	* Construtor da classe Livro. Sao passados todos os campos para a criacao do livro.
+	* 
+	* @param titulo 		o titulo do livro.
+	* @param nomeAutor 		o nome do autor do livro.
+	* @param dtPublicacao 	a data de publicacao do livro.
+	* @param cod 			o codigo do livro.
+	* @param genero 		o genero do livro.
+	* @param quantidade 	a quantidade de livros.
+	* @see Livro
+	* @since 1.0
+	*/
 	public Livro(String titulo, String nomeAutor, String dtPublicacao, String cod, String genero, int quantidade){
 		this.titulo = titulo;
 		this.nomeAutor = nomeAutor;
@@ -28,54 +52,132 @@ class Livro{
 		this.quantidade = quantidade;
 	}
 
+	/**
+	* Metodo para pegar o titulo do livro.
+	*
+	* @return 	o titulo do livro
+	* @since 	1.0
+	*/
 	public String getTitulo(){
 		return this.titulo;
 	}
 
+	/**
+	* Seta o titulo do livro.
+	*
+	* @param titulo 	o titulo do livro
+	* @since 			1.0
+	*/
 	public void setTitulo(String titulo){
 		this.titulo = titulo;
 	}
 
+	/**
+	* Metodo para pegar o nome do autor do livro.
+	*
+	* @return 	o nome do autor do livro
+	* @since 	1.0
+	*/
 	public String getNomeAutor(){
 		return this.nomeAutor;
 	}
 
+	/**
+	* Seta o nome do autor do livro.
+	*
+	* @param nomeAutor 	o nome do autor do livro
+	* @since 			1.0
+	*/
 	public void setNomeAutor(String nomeAutor){
 		this.nomeAutor = nomeAutor;
 	}
 
+	/**
+	* Metodo para pegar a data de publicacao do livro.
+	*
+	* @return 	a data de publicacao do livro
+	* @since 	1.0
+	*/
 	public String getDtPublicacao(){
 		return this.dtPublicacao;
 	}
 
+	/**
+	* Seta a data de publicacao do livro.
+	*
+	* @param dtPublicacao 	a data de publicacao do livro
+	* @since 				1.0
+	*/
 	public void setDtPublicacao(String dtPublicacao){
 		this.dtPublicacao = dtPublicacao;
 	}
 
+	/**
+	* Metodo para pegar o codigo do livro.
+	*
+	* @return 	o codigo do livro
+	* @since 	1.0
+	*/
 	public String getCod(){
 		return this.cod;
 	}
 
+	/**
+	* Seta o codigo do livro.
+	*
+	* @param cod 	o codigo do livro
+	* @since 		1.0
+	*/
 	public void setCod(String cod){
 		this.cod = cod;
 	}
 
+	/**
+	* Metodo para pegar o genero do livro.
+	*
+	* @return 	o genero do livro
+	* @since 	1.0
+	*/
 	public String getGenero(){
 		return this.genero;
 	}
 
+	/**
+	* Seta o genero do livro.
+	*
+	* @param genero		o genero do livro
+	* @since 			1.0
+	*/
 	public void setGenero(String genero){
 		this.genero = genero;
 	}
 
+	/**
+	* Metodo para pegar a quantidade de livros.
+	*
+	* @return 	a quantidade de livros
+	* @since 	1.0
+	*/
 	public int getQuantidade(){
 		return this.quantidade;
 	}
 
+	/**
+	* Seta a quantidade de livros.
+	*
+	* @param quantidade 	a quantidade de livros
+	* @since 				1.0
+	*/
 	public void setQuantidade(int quantidade){
 		this.quantidade = quantidade;
 	}
 
+	/**
+	* Retorna uma string com todos os campos do livro ordenados em "codigo;titulo;nome do autor;data de publicacao;genero;quantidade"
+	*
+	* @return 	uma string com todos os campos do livro
+	* @since 	1.0
+	*/
 	public String camposEmStr(){
 		String str = cod + ";" + titulo + ";" + nomeAutor + ";" + dtPublicacao + ";" + genero + ";" + quantidade;
 		return str;
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,14 @@
-//Tipo = adm (0) ou cliente (1)
 import java.util.List;
 
+/**
+* Classe que representa o objeto Pessoa. Acerca de cada pessoa, serão armazenados os seguintes dados: 
+* tipo (adm=0 ou cliente=1), nome, apelido, senha, estante e telefone.
+*
+* @author Andre Accioly
+* @author Tiago Kfouri
+* @version %I%, %G%
+* @since 1.0
+*/
 class Pessoa{
 	private int tipo;
 	private String nome;
@@ -9,6 +17,12 @@ class Pessoa{
 	private Estante estante;
 	private String telefone;
 
+	/**
+	* Construtor da classe Pessoa. Caso nao seja passado parametro, os campos serao iniciados com campos genericos
+	* 
+	* @see Pessoa
+	* @since 1.0
+	*/
 	public Pessoa(){
 		this.nome = "Nome generico";
 		this.tipo = 1;
@@ -18,6 +32,18 @@ class Pessoa{
 		this.telefone = "(00) - 000000000";
 	}
 
+	/**
+	* Construtor da classe Pessoa. Sao passados todos os campos para a criacao da pessoa.
+	* 
+	* @param nome 			o nome da pessoa.
+	* @param apelido 		o apelido da pessoa.
+	* @param senha 		 	a senha da pessoa.
+	* @param tipo 			o tipo da pessoa (adm=0 ou cliente=1).
+	* @param estante 		a estante da pessoa.
+	* @param telefone 		o telefone da pessoa.
+	* @see Pessoa
+	* @since 1.0
+	*/
 	public Pessoa(String nome, String apelido, String senha, int tipo, Estante estante, String telefone){
 		this.nome = nome;
 		this.apelido = apelido;
@@ -27,55 +53,135 @@ class Pessoa{
 		this.telefone = telefone;
 	}
 
-	public void setNome(String nome){
-		this.nome = nome;
-	}
-
+	/**
+	* Metodo para pegar o nome da pessoa.
+	*
+	* @return 	o nome da pessoa
+	* @since 	1.0
+	*/
 	public String getNome(){
 		return this.nome;
 	}
 
+	/**
+	* Seta o nome da pessoa.
+	*
+	* @param nome 	o nome da pessoa
+	* @since 		1.0
+	*/
+	public void setNome(String nome){
+		this.nome = nome;
+	}
 
+	/**
+	* Metodo para pegar a senha da pessoa.
+	*
+	* @return 	a senha da pessoa
+	* @since 	1.0
+	*/
 	public String getSenha(){
 		return this.senha;
 	}
 
+	/**
+	* Seta a senha da pessoa.
+	*
+	* @param senha 	a senha da pessoa
+	* @since 		1.0
+	*/
 	public void setSenha(String senha){
 		this.senha = senha;
 	}
 
+	/**
+	* Metodo para pegar o apelido da pessoa.
+	*
+	* @return 	o apelido da pessoa
+	* @since 	1.0
+	*/
 	public String getApelido(){
 		return this.apelido;
 	}
 
+	/**
+	* Seta o apelido da pessoa.
+	*
+	* @param apelido 	o apelido da pessoa
+	* @since 			1.0
+	*/
 	public void setApelido(String apelido){
 		this.apelido = apelido;
 	}
 
+	/**
+	* Metodo para pegar o tipo da pessoa.
+	*
+	* @return 	o tipo da pessoa
+	* @since 	1.0
+	*/
 	public int getTipo(){
 		return this.tipo;
 	}
 
+	/**
+	* Seta o tipo da pessoa.
+	*
+	* @param tipo 	o tipo da pessoa
+	* @since 		1.0
+	*/
 	public void setTipo(int tipo){
 		this.tipo = tipo;
 	}
 
+	/**
+	* Metodo para pegar a estante da pessoa.
+	*
+	* @return 	a estante da pessoa
+	* @since 	1.0
+	*/
 	public Estante getEstante(){
 		return this.estante;
 	}
 
+	/**
+	* Seta a estante da pessoa.
+	*
+	* @param estante 	a estante da pessoa
+	* @since 			1.0
+	*/
 	public void setEstante(Estante estante){
 		this.estante = estante;
 	}
 
-	public void setTelefone(String telefone){
-		this.telefone = telefone;
-	}
-
+	/**
+	* Metodo para pegar o telefone da pessoa.
+	*
+	* @return 	o telefone da pessoa
+	* @since 	1.0
+	*/
 	public String getTelefone(){
 		return this.telefone;
 	}
 
+	/**
+	* Seta o telefone da pessoa.
+	*
+	* @param telefone 	o telefone da pessoa
+	* @since 			1.0
+	*/
+	public void setTelefone(String telefone){
+		this.telefone = telefone;
+	}
+
+	/**
+	* Metodo que retorna todos os campos da pessoa, em formato de string, ordenados em 
+	* "nome;apelido;senha;tipo;telefone;codigos dos livros da estante"
+	*
+	* @return 	uma string com todos os campos da pessoa
+	* @see 		Estante
+	* @see 		codEmStr
+	* @since 	1.0
+	*/
 	public String camposEmStr(){
 		String str = nome + ";" + apelido + ";" + senha + ";" +  tipo + ";" + telefone + ";" + estante.codEmStr();
 		return str;
